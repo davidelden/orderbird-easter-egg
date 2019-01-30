df24e147168fef26e3e0799a27101e04611bf1ac
davidelden <deelden+github@gmail.com>
2019-01-30 10:58:53 -0600

    begin building module

---
+++ javascript/modules/EasterEggModule.js
@@ -0,0 +1,12 @@
+class EasterEggElement {
+  constructor() {
+    this.div = document.createElement('div');
+    this.div.setAttribute('id', 'easter-egg');
+  }
+
+  getDiv() {
+    return this.div;
+  }
+}
+
+export EasterEggElement;

d1d40060602ecbe5c26be7dc856c11aeb9f470a7
davidelden <deelden+github@gmail.com>
2019-01-30 14:07:55 -0600

    add ClickElements class to module

--- javascript/modules/EasterEggModule.js
+++ javascript/modules/EasterEggModule.js
@@ -9,4 +9,32 @@ class EasterEggElement {
   }
 }
 
-export default EasterEggElement;
+class ClickElements {
+  constructor() {
+    this.eggyClass = document.getElementsByClassName('eggy');
+    this.dataEggy = document.querySelectorAll('[data-eggy]');
+  }
+
+  removeInvalidElements(elements) {
+    let validElements = [];
+
+    for(let item of elements) {
+      let classes = item.classList;
+
+      if(classes.contains('disable') || item.disabled) {
+        continue;
+      }
+      validElements.push(item);
+    }
+    return validElements;
+  }
+
+  getValidElements() {
+    let arr1 = this.removeInvalidElements(this.eggyClass),
+        arr2 = this.removeInvalidElements(this.dataEggy);
+
+    return arr1.concat(arr2);
+  }
+}
+
+export { EasterEggElement, ClickElements };

128c29e8562c7128288a9c2c18f8445da799706d
davidelden <deelden+github@gmail.com>
2019-01-31 11:56:35 -0600

    add keydown listener for Konami code

--- javascript/insertEasterEgg.js
+++ javascript/insertEasterEgg.js
@@ -1,10 +1,21 @@
-import { EasterEggElement, ClickElements } from '/javascript/modules/EasterEggModule.js';
+import { EasterEggElement, ClickElements, KonamiCode } from '/javascript/modules/EasterEggModule.js';
 
-const easterEgg = new EasterEggElement;
-const clickElements = new ClickElements;
+const easterEgg = new EasterEggElement,
+      clickElements = new ClickElements,
+      konami = new KonamiCode;
+
 
 clickElements.getValidElements().map(element => {
   element.addEventListener('click', () => {
     document.body.appendChild(easterEgg.getDiv());
   });
+});
+
+document.addEventListener('keydown', e => {
+  konami.detectPattern(e.code);
+
+  if(konami.successfulMatch()) {
+    easterEgg.addToDOM();
+    konami.resetKeyPattern();
+  };
 });

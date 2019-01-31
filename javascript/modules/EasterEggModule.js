740353424697eeb823fa4d41738a97dcc45e65f0
davidelden <deelden+github@gmail.com>
2019-01-31 11:53:24 -0600

    add KonamiCode module

--- javascript/modules/EasterEggModule.js
+++ javascript/modules/EasterEggModule.js
@@ -37,4 +37,69 @@ class ClickElements {
   }
 }
 
-export { EasterEggElement, ClickElements };
+class KonamiCode {
+  constructor() {
+    this.konamiPattern = [
+      'ArrowUp',
+      'ArrowUp',
+      'ArrowDown',
+      'ArrowDown',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowLeft',
+      'ArrowRight',
+      'KeyB',
+      'KeyA'
+    ];
+    this.keyPattern = [];
+  }
+
+  getKonamiPattern() {
+    return this.konamiPattern;
+  }
+
+  getKeyPattern() {
+    return this.keyPattern;
+  }
+
+  getFirstThreeKonami() {
+    return this.konamiPattern.slice(0,3);
+  }
+
+  addKeyCode(keyCode) {
+    this.keyPattern.push(keyCode);
+  }
+
+  removeFirstKeyPattern() {
+    this.keyPattern.shift();
+  }
+
+  firstThreeMatch() {
+    return this.getKeyPattern().join() == this.getFirstThreeKonami().join() ? true : false;
+  }
+
+  successfulMatch() {
+    return this.getKeyPattern().join() == this.getKonamiPattern().join() ? true : false;
+  }
+
+  resetKeyPattern() {
+    this.keyPattern.length = 0;
+  }
+
+  detectPattern(keyCode) {
+    this.addKeyCode(keyCode);
+
+    if(!this.firstThreeMatch() && this.getKeyPattern().length == 3) {
+      this.removeFirstKeyPattern();
+    }
+
+    if(this.successfulMatch()) {
+      console.log('success: ', this.getKeyPattern());
+    } else if(this.getKeyPattern().length == this.getKonamiPattern().length) {
+      console.log('failed: ', this.getKeyPattern());
+      this.resetKeyPattern();
+    }
+  }
+}
+
+export { EasterEggElement, ClickElements, KonamiCode };
